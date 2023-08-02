324a335ec6c03080359fec7d400b79f3a163294c
Gourav <goupg1999@gmail.com>
2023-08-02 19:16:02 +0530

    navbar-search

--- client/src/layouts/Navbar.jsx
+++ client/src/layouts/Navbar.jsx
@@ -1,15 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { logoutUser } from "../redux/actions/auth";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../components/contexts/SearchContext";
 
 const Navbar = () => {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
+  const location = useLocation();
+  const [route, setRoute] = useState();
   const handleLogout = () => dispatch(logoutUser());
   const { setSearch } = useContext(SearchContext);
-
+  useEffect(() => {
+    setRoute(location.pathname);
+  });
+  console.log(route);
   return (
     <nav className="sticky top-0 z-[100] flex w-full items-center justify-between bg-neutral-100 py-3 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 lg:flex-wrap lg:justify-start">
       <div className="container flex items-center justify-between  px-4 mx-auto">
@@ -28,41 +33,43 @@ const Navbar = () => {
               PinCraft
             </h1>
           </Link>
-          <div className=" lg:flex-shrink lg:w-[60%] w-full mx-2 lg:mx-auto">
-            <label
-              htmlFor="default-search"
-              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
-            <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
+          {route === "/" && (
+            <div className=" lg:flex-shrink lg:w-[60%] w-full mx-2 lg:mx-auto">
+              <label
+                htmlFor="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
+                  </svg>
+                </div>
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search Mockups, Logos..."
+                  required=""
+                  onChange={(e) => setSearch(e.currentTarget.value)}
+                />
               </div>
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..."
-                required=""
-                onChange={(e) => setSearch(e.currentTarget.value)}
-              />
             </div>
-          </div>
+          )}
         </div>
         <div className="flex justify-between items-center">
           {auth.loaded && auth.token ? (
